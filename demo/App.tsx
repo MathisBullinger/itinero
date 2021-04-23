6006b1a90c502f9164a4b58e120b60b582561e44
Mathis Bullinger <mathis@bullinger.dev>
2021-04-23 18:56:24 +0200

    initial demo page

--- demo/App.tsx
+++ demo/App.tsx
@@ -1,9 +1,49 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Router from '../lib/Router'
+import { Switch, Route, Link, RouteProps } from '../lib'
+import './main.css'
 
 function App() {
-  return <Router></Router>
+  return (
+    <>
+      <Route path={/^\/(?!$)/}>
+        <Link to="/" className="back">
+          /
+        </Link>
+      </Route>
+      <Switch>
+        <Route path={/^\/$/}>{Home}</Route>
+        <Route path={/^\/[a-d]$/}>{Alpha}</Route>
+        <Route path={/foo/}>
+          <Foo>bar</Foo>
+        </Route>
+      </Switch>
+    </>
+  )
+}
+
+function Home() {
+  console.log('home')
+  return (
+    <div className="home screen">
+      {['a', 'b', 'c', 'd'].map(v => (
+        <Link key={v} to={v} className={`card cl-${v}`}>
+          {v}
+        </Link>
+      ))}
+    </div>
+  )
+}
+
+const Alpha: React.FC<RouteProps> = ({ location }) => {
+  const char = location.path.slice(1)
+  console.log('alpha', char)
+  return <div className={`screen cl-${char}`}>{char}</div>
+}
+
+function Foo({ children }: { children?: string }) {
+  console.log('render foo')
+  return <div>foo {children}</div>
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
